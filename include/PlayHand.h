4d2b37e026fedfb1ed65a409cebafc8924f6d327
yy c <threeicream@outlook.com>
2025-04-14 22:46:36 +0800

    完成judgeCardType函数，进行牌型分类

--- include/PlayHand.h
+++ include/PlayHand.h
@@ -35,16 +35,36 @@ public:
 		Hand_Bomb_Jokers_Two_Single//王炸带两单
 	};
 public:
+	//得到牌型的属性信息
+	PlayHand::HandType getType();
+	Card::CardPoint getPt();
+	int getExtra();
+private:
 	//1.对扑克牌进行分类：1张的，2张，3张，4张有多少种组合
 	void classify(Cards& cards);
 
 	//2.对牌型进行分类
 	void judgeCardType();
 
-	//得到牌型的属性信息
-	PlayHand::HandType getType();
-	Card::CardPoint getPt();
-	int getExtra();
+	//判断牌的类型
+	bool isSingle();//单
+	bool isPair();//对
+	bool isTriple();//三个
+	bool isTripleSingle();//三带一
+	bool isTriplePair();//三带二
+	bool isPlane();//飞机 555_666
+	bool isPlaneTwoSingle();//飞机带单 555_666_3_4
+	bool isPlaneTwoPair();//飞机带双 555_666_33_44
+	bool isHandSeqPair();//连对 33_44_55
+	bool isHandSeqSingle();//顺子 34567
+	bool isBomb();//炸弹
+	bool isBombSingle();//炸弹带一个
+	bool isBombPair();//炸弹带一对
+	bool isBombTwoSingle();//炸弹带两单
+	bool isBombJokers();//王炸
+	bool isBombJokersSingle();//王炸带一个
+	bool isBombJokersPair();//王炸带一对
+	bool isBombJokersTwoSingle();//王炸带两单
 public:
 	PlayHand();
 	//传递给类一组牌，通过类分析出牌型，点数，以及相关的附属信息（比如顺子：记录牌的数量）
--- src/PlayHand.cpp
+++ src/PlayHand.cpp
@@ -36,6 +36,85 @@ void PlayHand::classify(Cards& cards)
 
 void PlayHand::judgeCardType()
 {
+	m_type = Hand_Unknown;
+	m_pt = Card::Card_Begin;
+	m_extra = 0;
+
+	// 按照优先级顺序判断牌型
+	if (isBombJokersTwoSingle()) {
+		m_type = Hand_Bomb_Jokers_Two_Single;
+	}
+	else if (isBombJokersPair()) {
+		m_type = Hand_Bomb_Jokers_Pair;
+	}
+	else if (isBombJokersSingle()) {
+		m_type = Hand_Bomb_Jokers_Single;
+	}
+	else if (isBombJokers()) {
+		m_type = Hand_Bomb_Jokers;
+		//王炸没有点数
+	}
+	else if (isBombTwoSingle()) {
+		m_type = Hand_Bomb_Two_Single;
+		m_pt = m_fourCard.first();//炸弹大小由炸弹本身决定
+	}
+	else if (isBombPair()) {
+		m_type = Hand_Bomb_Pair;
+		m_pt = m_fourCard.first();
+	}
+	else if (isBombSingle()) {
+		m_type = Hand_Bomb_Single;
+		m_pt = m_fourCard.first();
+	}
+	else if (isBomb()) {
+		m_type = Hand_Bomb;
+		m_pt = m_fourCard.first();//记录炸弹的大小
+	}
+	else if (isHandSeqSingle()) {
+		m_type = Hand_Hand_Seq_Single;
+		m_pt = m_oneCard.first();//记录最小的点数
+		m_extra = m_oneCard.size();//记录顺子的长度
+	}
+	else if (isHandSeqPair()) {
+		m_type = Hand_Hand_Seq_Pair;
+		m_pt = m_twoCard.first();//记录最小的点数
+		m_extra = m_twoCard.size();//记录连对的长度
+	}
+	else if (isPlaneTwoPair()) {
+		m_type = Hand_Plane_Two_Pair;
+		m_pt = m_threeCard.first();//记录最小的点数
+	}
+	else if (isPlaneTwoSingle()) {
+		m_type = Hand_Plane_Two_Single;
+		m_pt = m_threeCard.first();
+	}
+	else if (isPlane()) {
+		m_type = Hand_Plane;
+		m_pt = m_threeCard.first(); //记录飞机最小的牌点
+	}
+	else if (isTriplePair()) {
+		m_type = Hand_Triple_Pair;
+		m_pt = m_threeCard.first();
+	}
+	else if (isTripleSingle()) {
+		m_type = Hand_Triple_Single;
+		m_pt = m_threeCard.first();
+	}
+	else if (isTriple()) {
+		m_type = Hand_Triple;
+		m_pt = m_threeCard.first(); // 记录三个的点数
+	}
+	else if (isPair()) {
+		m_type = Hand_Pair;
+		m_pt = m_twoCard.first(); // 记录对子的点数
+	}
+	else if (isSingle()) {
+		m_type = Hand_Single;
+		m_pt = m_oneCard.first(); // 记录单张的点数
+	}
+	else {
+		m_type = Hand_Unknown; // 如果没有匹配的牌型，则保持 Hand_Unknown
+	}
 }
 
 PlayHand::HandType PlayHand::getType()
@@ -51,6 +130,181 @@ Card::CardPoint PlayHand::getPt()
 int PlayHand::getExtra()
 {
 	return m_extra;
+}
+
+bool PlayHand::isSingle()
+{
+	if (m_oneCard.size() == 1 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.isEmpty()) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isPair()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.size() == 1 && m_threeCard.isEmpty() && m_fourCard.isEmpty()) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isTriple()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.isEmpty() && m_threeCard.size() == 1 && m_fourCard.isEmpty()) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isTripleSingle()
+{
+	if (m_oneCard.size() == 1 && m_twoCard.isEmpty() && m_threeCard.size() == 1 && m_fourCard.isEmpty()) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isTriplePair()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.size() == 1 && m_threeCard.size() == 1 && m_fourCard.isEmpty()) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isPlane()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.isEmpty() && m_threeCard.size() == 2 && m_fourCard.isEmpty()) {
+		std::sort(m_threeCard.begin(), m_threeCard.end());
+		if (m_threeCard[1] - m_threeCard[0] == 1 && m_threeCard[1] < Card::Card_2) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PlayHand::isPlaneTwoSingle()
+{
+	if (m_oneCard.size() == 2 && m_twoCard.isEmpty() && m_threeCard.size() == 2 && m_fourCard.isEmpty()) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		std::sort(m_threeCard.begin(), m_threeCard.end());
+		if (m_oneCard[0] == Card::Card_SJ && m_oneCard[1] == Card::Card_BJ) {
+			return false;
+		}
+		if (m_threeCard[1] - m_threeCard[0] == 1 && m_threeCard[1] < Card::Card_2) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PlayHand::isPlaneTwoPair()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.size() == 1 && m_threeCard.size() == 2 && m_fourCard.isEmpty()) {
+		std::sort(m_threeCard.begin(), m_threeCard.end());
+		if (m_threeCard[1] - m_threeCard[0] == 1 && m_threeCard[1] < Card::Card_2) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PlayHand::isHandSeqPair()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.size() >= 3 && m_threeCard.isEmpty() && m_fourCard.isEmpty()) {
+		std::sort(m_twoCard.begin(), m_twoCard.end());
+		if (m_twoCard.last() - m_twoCard.first() == (m_twoCard.size() - 1) 
+			&& m_twoCard.first() >= Card::Card_3 && m_twoCard.last() < Card::Card_2) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PlayHand::isHandSeqSingle()
+{
+	if (m_oneCard.size() >= 5 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.isEmpty()) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		if (m_oneCard.last() - m_oneCard.first() == (m_oneCard.size() - 1)
+			&& m_oneCard.first() >= Card::Card_3 && m_oneCard.last() < Card::Card_2) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PlayHand::isBomb()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombSingle()
+{
+	if (m_oneCard.size() == 1 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombPair()
+{
+	if (m_oneCard.isEmpty() && m_twoCard.size() == 1 && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombTwoSingle()
+{
+	if (m_oneCard.size() == 2 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		if (m_oneCard[0] != Card::Card_SJ && m_oneCard[1] != Card::Card_BJ)
+			return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombJokers()
+{
+	if (m_oneCard.size() == 2 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		if (m_oneCard[0] == Card::Card_SJ && m_oneCard[1] == Card::Card_BJ)
+			return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombJokersSingle()
+{
+	if (m_oneCard.size() == 3 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		if (m_oneCard[1] == Card::Card_SJ && m_oneCard[2] == Card::Card_BJ)
+			return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombJokersPair()
+{
+	if (m_oneCard.size() == 2 && m_twoCard.size() == 1 && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		if (m_oneCard[0] == Card::Card_SJ && m_oneCard[1] == Card::Card_BJ)
+			return true;
+	}
+	return false;
+}
+
+bool PlayHand::isBombJokersTwoSingle()
+{
+	if (m_oneCard.size() == 4 && m_twoCard.isEmpty() && m_threeCard.isEmpty() && m_fourCard.size() == 1) {
+		std::sort(m_oneCard.begin(), m_oneCard.end());
+		if (m_oneCard[2] == Card::Card_SJ && m_oneCard[3] == Card::Card_BJ)
+			return true;
+	}
+	return false;
 }
 
 PlayHand::PlayHand()
